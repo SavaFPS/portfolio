5dfb8cf863b4ff12ff98cad22cfca8fc1ce5ce79
unknown <sava.tasic23@gmail.com>
2024-08-08 17:49:58 +0200

    Init Portfolio

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+// import Link from 'next/link';
+// import { Button } from './ui/button';
+
+export const Header = () => {
+  return (
+    <header className="py-8 xl:py-12 text-white bg-green-500/20">
+      <div className="container mx-auto">HEADER</div>
+    </header>
+  );
+};
